4af788b6ebf3fdf8c3711495f8a349f1d9abd7c7
zahiruldu <zahirul.haque@aurea.com>
2019-08-23 12:06:58 +0600

    multi page routing

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4395cfa1efee99ac2c38";
+/******/ 	var hotCurrentHash = "0584bcc905dc5e98c4d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
